b1e226373f03e10529e30c514553bcc9a8ecc1da
Asad Ul Haq <ashaq.bee16seecs@seecs.edu.pk>
2021-01-19 00:10:16 +0500

    Update code.java

--- code.java
+++ code.java
@@ -4,7 +4,7 @@ public class code{
 	public static void main(String[] args){
 		String fonts[] = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
 		
-		for ( int i = 0; i < fonts.length; i++ )
+		for(int i = 0; i < fonts.length; i++)
 			System.out.println(fonts[i]);
 	}
 }
